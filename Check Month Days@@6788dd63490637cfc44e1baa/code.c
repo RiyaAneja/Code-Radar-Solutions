8fbdfd557d57be5b40fee58daf8ec2fe9ed3413e
RiyaAneja <riya.aneja07@gmail.com>
2025-01-31 17:00:59 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -2,54 +2,46 @@
 int main(){
     int a,i;
     scanf("%d %d",&a,&i);
-    switch(a%4==0&&a%100!=0||a%400==0){
+    switch(i)
+    {
         case 1:
-        switch(i)
-        {
-            case 1:
             printf("31");
             break;
-            case 2:
-            printf("29");
-            break;
-            case 3:
-            printf("31");
-            break;
-            case 4:
-            printf("30");
-            break;
-            case 5:
-            printf("31");
-            break;
-            case 6:
-            printf("30");
-            break;
-            case 7:
-            printf("31");
-            break;
-            case 8:
-            printf("31");
-            break;
-            case 9:
-            printf("30");
-            break;
-            case 10:
-            printf("31");
-            break;
-            case 11:
-            printf("30");
-            break;
-            case 12:
-            printf("31");
-            break;
-        }
-        break;
-        default:
-        {
-            case 2:
+        case 2:
             printf("28");
             break;
-        }
+        case 3:
+            printf("31");
+            break;
+        case 4:
+            printf("30");
+            break;
+        case 5:
+            printf("31");
+            break;
+        case 6:
+            printf("30");
+            break;
+        case 7:
+            printf("31");
+            break;
+        case 8:
+            printf("31");
+            break;
+        case 9:
+            printf("30");
+            break;
+        case 10:
+            printf("31");
+            break;
+        case 11:
+            printf("30");
+            break;
+        case 12:
+            printf("31");
+            break;
+        default:
+            printf("Invalid month");
     }
     
     return 0;
