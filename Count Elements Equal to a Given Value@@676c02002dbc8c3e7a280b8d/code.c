91a923fc84a55740e281b74fae4fa9a42a7da5bb
Riya <riya.aneja07@gmail.com>
2025-03-28 02:26:07 +0530

    Solution to Count Elements Equal to a Given Value

--- Count Elements Equal to a Given Value@@676c02002dbc8c3e7a280b8d/code.c
+++ Count Elements Equal to a Given Value@@676c02002dbc8c3e7a280b8d/code.c
@@ -1,43 +1,22 @@
-// #include<stdio.h>
-// int main(){
-//     int n,k;
-//     scanf("%d",&n);
-//     scanf("%d",&k);
-//     int arr[n];
-//     for(int b=0;b<n;b++){
-//         scanf("%d",&arr[b]);
-//     }
-//     for(int i=0;i<n;i++){
-//         int count=0;
-//         for(int j=i+1;j<n;j++){
-//             if(arr[i]+arr[j]==k){
-//                 count+=1;
-//             }  
-            
-//         } 
-         
-//     }
-//     printf("%d",count); 
-//     return 0;
-// }
 #include<stdio.h>
-
 int main(){
-    int n, k;
-    scanf("%d", &n);
-    scanf("%d", &k);
+    int n,k;
+    scanf("%d",&n);
+    scanf("%d",&k);
     int arr[n];
-    for(int b = 0; b < n; b++){
-        scanf("%d", &arr[b]);
+    for(int b=0;b<n;b++){
+        scanf("%d",&arr[b]);
     }
-    int count = 0; // Move count initialization outside the loop
-    for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
-            if(arr[i] + arr[j] == k){
-                count += 1;
-            }
+    int count=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==k){
+                count+=1;
+            }  
+            
         } 
+    printf("%d",count); 
     }
-    printf("%d", count); 
+    
     return 0;
 }
