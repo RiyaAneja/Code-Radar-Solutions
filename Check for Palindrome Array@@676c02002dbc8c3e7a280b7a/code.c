3099a7a61f7fa5b35199bf7b9751daa0a6d76b58
Riya <riya.aneja07@gmail.com>
2025-03-28 02:51:12 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,31 +1,29 @@
-#include<stdio.h>
-void palimdrome(int arr[],int n){
-    for(int i=0;i<n;i++){
-        arr[i]=arr[n-i-1];
-    }
-    return;
-}
-int comp(int arr[],int aa[],int n){
-    for(int i=0;i<n;i++){
-        if(arr[i]!=aa[i]){
-            return 0;
+#include <stdio.h>
+
+int main() {
+    int n, t; 
+    scanf("%d", &n); // Read the number of elements
+
+    int arr[n]; 
+    for (int b = 0; b < n; b++) { 
+        scanf("%d", &arr[b]); // Read the elements of the array
+    } 
+    
+    scanf("%d", &t); // Read the target sum
+
+    int found = 0; // Flag to check if any pair is found
+    for (int i = 0; i < n; i++) { 
+        for (int j = i + 1; j < n; j++) { 
+            if (arr[i] + arr[j] == t) {
+                printf("%d %d\n", i + 1, j + 1); // Print 1-based indices
+                found = 1; // Set the flag to indicate a pair was found
+            }
         }
-        return 1;
     }
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    int aa[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+    if (!found) {
+        printf("No pairs found that sum to %d\n", t); // Inform if no pairs are found
     }
-    if(comp(arr,aa,n)){
-        printf("YES");
-    }
-    else{
-        printf("NO");
-    }
+
     return 0;
 }
