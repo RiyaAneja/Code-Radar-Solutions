40b79c0da5126b08a11594ff0ea77244b9f3a71e
Riya <riya.aneja07@gmail.com>
2025-03-28 00:45:30 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,58 +1,61 @@
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     int a=-1;
-    
-//     scanf("%d\n",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     int max=arr[0];
-//     for(int j=1;j<n;j++){
-//         if(max<arr[j]){
-//             max=arr[j];
-//             if(max>arr[j+1]){
-//                 break;
-//             }
-//         }
-         
-         
-//     }
-//     printf("%d",max); 
-    
-
-//     return 0;
-// }
 #include<stdio.h>
-
-int main() {
+int main(){
     int n;
-    int a = -1;
+    int a=-1;
     
-    scanf("%d", &n);
+    scanf("%d\n",&n);
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    // If the maximum number and the second element are the same
-    if (n > 1 && arr[1] == arr[0]) {
-        printf("-1");
-        return 0;
+    if(a>1 && arr[1]==arr[0]){
+        printf("%d",a);
     }
-
-    int max = arr[0];
-    for (int j = 1; j < n; j++) {
-        if (max < arr[j]) {
-            max = arr[j];
-            if (j + 1 < n && max > arr[j + 1]) {
+    int max=arr[0];
+    for(int j=1;j<n;j++){
+        if(max<arr[j]){
+            max=arr[j];
+            if(max>arr[j+1]){
                 break;
             }
         }
+         
+         
     }
-
-    printf("%d", max); 
+    printf("%d",max); 
+    
 
     return 0;
 }
+// #include<stdio.h>
+
+// int main() {
+//     int n;
+//     int a = -1;
+    
+//     scanf("%d", &n);
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     // If the maximum number and the second element are the same
+//     if (n > 1 && arr[1] == arr[0]) {
+//         printf("-1");
+//         return 0;
+//     }
+
+//     int max = arr[0];
+//     for (int j = 1; j < n; j++) {
+//         if (max < arr[j]) {
+//             max = arr[j];
+//             if (j + 1 < n && max > arr[j + 1]) {
+//                 break;
+//             }
+//         }
+//     }
+
+//     printf("%d", max); 
+
+//     return 0;
+// }
