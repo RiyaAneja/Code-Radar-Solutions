4b87456c25e71c0256a8dffedb3e028220d6c454
Riya <riya.aneja07@gmail.com>
2025-03-27 21:12:58 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,21 +1,42 @@
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int a=arr[0];
-    for(int j=1;j<n;j++){
-        if(a<arr[j]){
-            a=arr[j];  
-        }
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int a=arr[0];
+//     for(int j=1;j<n;j++){
+//         if(a<arr[j]){
+//             a=arr[j];  
+//         }
         
         
          
-    }
+//     }
   
-    printf("%d",a);
+//     printf("%d",a);
+//     return 0;
+// }
+#include<stdio.h>
+
+int main() {
+    int n, largest;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0]; // Assume the first element is the largest
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            largest = arr[i]; // Update if a larger element is found
+        }
+    }
+
+    printf("%d", largest); // Print the largest element
     return 0;
 }
