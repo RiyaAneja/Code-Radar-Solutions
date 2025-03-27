84d0a6dcc61ffc19fae1c4a081940932db57a77e
Riya <riya.aneja07@gmail.com>
2025-03-28 02:26:51 +0530

    Solution to Count Elements Equal to a Given Value

--- Count Elements Equal to a Given Value@@676c02002dbc8c3e7a280b8d/code.c
+++ Count Elements Equal to a Given Value@@676c02002dbc8c3e7a280b8d/code.c
@@ -1,22 +1,48 @@
-#include<stdio.h>
-int main(){
-    int n,k;
-    scanf("%d",&n);
-    scanf("%d",&k);
-    int arr[n];
-    for(int b=0;b<n;b++){
-        scanf("%d",&arr[b]);
-    }
-    int count=0;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]+arr[j]==k){
-                count+=1;
-            }  
+// #include<stdio.h>
+// int main(){
+//     int n,k;
+//     scanf("%d",&n);
+//     scanf("%d",&k);
+//     int arr[n];
+//     for(int b=0;b<n;b++){
+//         scanf("%d",&arr[b]);
+//     }
+//     int count=0;
+//     for(int i=0;i<n;i++){
+//         for(int j=i+1;j<n;j++){
+//             if(arr[i]+arr[j]==k){
+//                 count+=1;
+//             }  
             
-        } 
-    printf("%d",count); 
+//         } 
+//     printf("%d",count); 
+//     }
+    
+//     return 0;
+// }
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+    scanf("%d", &k);
+    
+    int arr[n];
+    for (int b = 0; b < n; b++) {
+        scanf("%d", &arr[b]);
     }
+    
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == k) {
+                count += 1;
+            }
+        }
+    }
+    
+    // Print the count of pairs that sum to k
+    printf("%d\n", count);
     
     return 0;
 }
