e3b5e43bd8df01d60f2498ccbf944b0620131359
Riya <riya.aneja07@gmail.com>
2025-03-28 02:36:06 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,21 +1,47 @@
-#include<stdio.h> 
-int main(){ 
-    int n,t; 
-    scanf("%d",&n); 
+// #include<stdio.h> 
+// int main(){ 
+//     int n,t; 
+//     scanf("%d",&n); 
     
-    int arr[n]; 
-    for(int b=0;b<n;b++){ 
-        scanf("%d",&arr[b]); 
-    } 
-    scanf("%d",&t); 
+//     int arr[n]; 
+//     for(int b=0;b<n;b++){ 
+//         scanf("%d",&arr[b]); 
+//     } 
+//     scanf("%d",&t); 
     
-    for(int i=0;i<n;i++){ 
-        for(int j=i+1;j<n;j++){ 
-            if(arr[i]+arr[j]==t){
-                printf("%d %d\n",i+1,j+1);
+//     for(int i=0;i<n;i++){ 
+//         for(int j=i+1;j<n;j++){ 
+//             if(arr[i]+arr[j]==t){
+//                 printf("%d %d\n",i+1,j+1);
                  
+//             }
+//         }
+//     }
+//     return 0;
+// }
+#include <stdio.h>
+
+int main() {
+    int n, t; 
+    scanf("%d", &n); // Read the number of elements
+
+    int arr[n]; 
+    for (int b = 0; b < n; b++) { 
+        scanf("%d", &arr[b]); // Read the elements of the array
+    } 
+    
+    scanf("%d", &t); // Read the target sum
+
+    int found = 0; // Flag to check if any pair is found
+    for (int i = 0; i < n; i++) { 
+        for (int j = i + 1; j < n; j++) { 
+            if (arr[i] + arr[j] == t) {
+                printf("%d %d\n", i + 1, j + 1); // Print 1-based indices
+                found = 1; // Set the flag to indicate a pair was found
             }
         }
     }
+
+   
     return 0;
 }
