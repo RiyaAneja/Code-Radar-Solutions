943df314ad996fdff16bd514b0dcd943f4f1c55d
Riya <riya.aneja07@gmail.com>
2025-03-28 17:10:40 +0530

    Solution to Find the Sum of Digits of Each Element in an Array

--- Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
+++ Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
@@ -1,29 +1,59 @@
+// #include<stdio.h>
+// int cal(int num){
+//     int n,sum=0;
+//     while(num!=0){
+//         n=n%10;
+//         sum+=n;
+//         num=num/10;        
+//     }
+//     return sum;
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n],arrr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=0;i<n;i++){
+//         int summ=cal(arr[i]);
+//         for(int j=0;j<n;j++){
+//             arrr[j]=summ;
+//         }
+//     }
+//     for(int j=0;j<n;j++){
+//         printf("%d ",&arr[j]);
+//     }
+//     return 0;  
+// }
 #include<stdio.h>
-int cal(int num){
-    int n,sum=0;
-    while(num!=0){
-        n=n%10;
-        sum+=n;
-        num=num/10;        
+
+int cal(int num) {
+    int n, sum = 0;
+    while (num != 0) {
+        n = num % 10;  // Fixing incorrect usage of `n`
+        sum += n;
+        num = num / 10;        
     }
     return sum;
 }
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
-    int arr[n],arrr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    scanf("%d", &n);
+
+    int arr[n], arrr[n];  // Arrays to store numbers and results
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  // Reading array elements
     }
-    for(int i=0;i<n;i++){
-        int summ=cal(arr[i]);
-        for(int j=0;j<n;j++){
-            arrr[j]=summ;
-        }
+
+    for (int i = 0; i < n; i++) {
+        arrr[i] = cal(arr[i]); // Calculating and storing digit sum of each element
     }
-    for(int j=0;j<n;j++){
-        printf("%d ",&arr[j]);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arrr[i]);  // Printing the results
     }
-    return 0;
-    
+
+    return 0;  
 }
