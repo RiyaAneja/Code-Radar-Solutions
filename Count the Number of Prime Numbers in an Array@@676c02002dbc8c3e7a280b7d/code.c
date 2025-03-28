1b4f37866dd41904c2e02d36d21d1f9f88789468
Riya <riya.aneja07@gmail.com>
2025-03-28 16:46:16 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -1,25 +1,52 @@
-#include<stdio.h>
-int prime(int arr[],n){
-    int prime=0;
-    for(int i=0;i<n;i++){
-        for(int j=2;j<=n;j++){
-            if(arr[i]%2!=0){
-                prime=1;
-            }
-            break;
+// #include<stdio.h>
+// int prime(int num){
+//     int prime=0;
+//     for(int i=2;i<n;i++){
+//         if(num%2!=0){
+//             prime=1;
+//         }
+//         }
+//     }
+//     return i;
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int isprime=prime(arr[n],n);
+//     printf("%d",i);
+    
+//     return 0;
+// }
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0; // Numbers less than 2 are not prime
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0; // Divisible by another number
+    }
+    return 1; // Number is prime
+}
+
+int main() {
+    int n;
+    scanf("%d", &n); // Read the size of the array
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]); // Read the array elements
+    }
+
+    printf("Prime numbers in the array: ");
+    for (int i = 0; i < n; i++) {
+        if (isPrime(arr[i])) {
+            printf("%d ", arr[i]); // Print prime numbers
         }
     }
-    return i;
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int isprime=prime(arr[n],n);
-    printf("%d",i);
+    printf("\n");
     
     return 0;
 }
