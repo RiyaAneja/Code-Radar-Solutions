5a5ecf640ba1010978c3109371458ee1760dd81b
Riya <riya.aneja07@gmail.com>
2025-03-28 16:48:03 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -21,32 +21,61 @@
     
 //     return 0;
 // }
+// #include <stdio.h>
+
+// int isPrime(int num) {
+//     if (num <= 1) return 0; // Numbers less than 2 are not prime
+//     for (int i = 2; i * i <= num; i++) {
+//         if (num % i == 0) return 0; // Divisible by another number
+//     }
+//     return 1; // Number is prime
+// }
+
+// int main() {
+//     int n;
+//     scanf("%d", &n); // Read the size of the array
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]); // Read the array elements
+//     }
+
+//     printf("Prime numbers in the array: ");
+//     for (int i = 0; i < n; i++) {
+//         if (isPrime(arr[i])) {
+//             printf("%d ", arr[i]); // Print prime numbers
+//         }
+//     }
+//     printf("\n");
+    
+//     return 0;
+// }
 #include <stdio.h>
 
 int isPrime(int num) {
     if (num <= 1) return 0; // Numbers less than 2 are not prime
     for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) return 0; // Divisible by another number
+        if (num % i == 0) return 0; // Not prime if divisible
     }
     return 1; // Number is prime
 }
 
 int main() {
-    int n;
+    int n, primeCount = 0; // Variable to count the primes
     scanf("%d", &n); // Read the size of the array
 
     int arr[n];
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]); // Read the array elements
+        scanf("%d", &arr[i]); // Read array elements
     }
 
-    printf("Prime numbers in the array: ");
     for (int i = 0; i < n; i++) {
         if (isPrime(arr[i])) {
-            printf("%d ", arr[i]); // Print prime numbers
+            primeCount++; // Increment count for each prime number
         }
     }
-    printf("\n");
-    
+
+    printf("Total number of prime numbers: %d\n", primeCount);
+
     return 0;
 }
