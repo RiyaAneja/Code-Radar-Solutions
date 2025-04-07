9c0e0a3d68b59ccc8ceaf9ee527114c159fdddcb
Riya <riya.aneja07@gmail.com>
2025-04-07 16:52:02 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,24 +1,76 @@
+// #include<stdio.h>
+// int majority(int arr[],int n){
+//     int count=0;
+//     for(int i=0;i<n;i++){
+//         for(int j=i+1;j<n;j++){
+//             if(arr[i]==arr[j]){
+//                 count+=1;
+//             }
+//         }
+//     }
+//     return count;
+// }
+// int main(){
+//     int n,arr[n];
+//     scanf("%d",&n);
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int maj=majority(arr[n],n);
+//     if(maj>(n/2)){
+//         printf("%d",maj);
+//     }
+//     else printf("-1");
+// }
 #include<stdio.h>
-int majority(int arr[],int n){
-    int count=0;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]){
-                count+=1;
-            }
+
+int majority(int arr[], int n) {
+    int candidate = -1, count = 0;
+
+    // Step 1: Finding a candidate for the majority element
+    for (int i = 0; i < n; i++) {
+        if (count == 0) {
+            candidate = arr[i];
+            count = 1;
+        } else if (arr[i] == candidate) {
+            count++;
+        } else {
+            count--;
         }
     }
-    return count;
+
+    // Step 2: Verifying the candidate
+    count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == candidate) {
+            count++;
+        }
+    }
+
+    if (count > n / 2) {
+        return candidate; // Majority element
+    } else {
+        return -1; // No majority element
+    }
 }
-int main(){
-    int n,arr[n];
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int maj=majority(arr[n],n);
-    if(maj>(n/2)){
-        printf("%d",maj);
+
+    int maj = majority(arr, n);
+    if (maj != -1) {
+        printf("The majority element is: %d\n", maj);
+    } else {
+        printf("No majority element found.\n");
     }
-    else printf("-1");
+
+    return 0;
 }
