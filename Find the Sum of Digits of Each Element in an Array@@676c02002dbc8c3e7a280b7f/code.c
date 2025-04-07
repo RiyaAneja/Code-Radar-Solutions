1a11f7e863911b7edc31dfff65481ace69ed74b1
Riya <riya.aneja07@gmail.com>
2025-04-07 17:29:58 +0530

    Solution to Find the Sum of Digits of Each Element in an Array

--- Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
+++ Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
@@ -1,40 +1,41 @@
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     int sum=0;
-//     for(int i=0;i<n;i++){
-//         int digit=arr[i];
-//         while(digit!=0){
-//             sum=sum+digit%10;
-//             digit=digit/10;            
-//         }
-//         printf("%d",sum);        
-//     }
-    
-//     return 0;
-// }
 #include<stdio.h>
-
-int main() {
+int main(){
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n);
     int arr[n];
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-    for(int i = 0; i < n; i++) {
-        int digit = arr[i];
-        int sum = 0; // Reset the sum for each number
-        while(digit != 0) {
-            sum = sum + digit % 10;
-            digit = digit / 10;            
+   
+    for(int i=0;i<n;i++){
+        int sum=0;
+        int digit=arr[i];
+        while(digit!=0){
+            sum=sum+digit%10;
+            digit=digit/10;            
         }
-        printf("%d ", sum); // Print the sum of digits for the current number
+        printf("%d",sum);        
     }
+    
     return 0;
 }
+// #include<stdio.h>
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for(int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+//     for(int i = 0; i < n; i++) {
+//         int digit = arr[i];
+//         int sum = 0; // Reset the sum for each number
+//         while(digit != 0) {
+//             sum = sum + digit % 10;
+//             digit = digit / 10;            
+//         }
+//         printf("%d ", sum); // Print the sum of digits for the current number
+//     }
+//     return 0;
+// }
