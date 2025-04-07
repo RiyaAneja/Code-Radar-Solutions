14d41431742863c0775bc10708388ebb330afe53
Riya <riya.aneja07@gmail.com>
2025-04-07 17:25:56 +0530

    Solution to Find the Sum of Digits of Each Element in an Array

--- Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
+++ Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
@@ -1,59 +1,19 @@
-// #include<stdio.h>
-// int cal(int num){
-//     int n,sum=0;
-//     while(num!=0){
-//         n=n%10;
-//         sum+=n;
-//         num=num/10;        
-//     }
-//     return sum;
-// }
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n],arrr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     for(int i=0;i<n;i++){
-//         int summ=cal(arr[i]);
-//         for(int j=0;j<n;j++){
-//             arrr[j]=summ;
-//         }
-//     }
-//     for(int j=0;j<n;j++){
-//         printf("%d ",&arr[j]);
-//     }
-//     return 0;  
-// }
 #include<stdio.h>
-
-int cal(int num) {
-    int n, sum = 0;
-    while (num != 0) {
-        n = num % 10;  // Fixing incorrect usage of `n`
-        sum += n;
-        num = num / 10;        
-    }
-    return sum;
-}
-
-int main() {
+int main(){
     int n;
-    scanf("%d", &n);
-
-    int arr[n], arrr[n];  // Arrays to store numbers and results
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);  // Reading array elements
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    for (int i = 0; i < n; i++) {
-        arrr[i] = cal(arr[i]); // Calculating and storing digit sum of each element
+    int digit=arr[i],sum=0;
+    for(int i=0;i<n;i++){
+        while(digit!=0){
+            sum=sum+digit%10;
+            digit=digit/10;            
+        }
+        printf("%d",sum);        
     }
-
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arrr[i]);  // Printing the results
-    }
-
-    return 0;  
+    i++;
+    return 0;
 }
