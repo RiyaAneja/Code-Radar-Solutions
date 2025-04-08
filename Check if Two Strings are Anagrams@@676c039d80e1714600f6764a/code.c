06450acc16fc18594b46df6bf3a7d68968b81d6f
Riya <riya.aneja07@gmail.com>
2025-04-08 18:24:54 +0530

    Solution to Check if Two Strings are Anagrams

---
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+void countFrequency(char *str, int *freq) {
+    while (*str) {
+        freq[(int)(*str)]++; 
+        str++;
+    }
+}
+bool areAnagrams(char *str1, char *str2) {
+    if (strlen(str1) != strlen(str2)) {
+        return false;
+    }
+    int freq1[256] = {0};
+    int freq2[256] = {0};
+    countFrequency(str1, freq1);
+    countFrequency(str2, freq2);
+    for (int i = 0; i < 256; i++) {
+        if (freq1[i] != freq2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    char str1[100], str2[100];
+    fgets(str1, sizeof(str1), stdin);
+    str1[strcspn(str1, "\n")] = 0;
+    fgets(str2, sizeof(str2), stdin);
+    str2[strcspn(str2, "\n")] = 0;
+    if (areAnagrams(str1, str2)) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    return 0;
+}
