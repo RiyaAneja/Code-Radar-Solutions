18ac5e501ff6e94db35d6547313fcc3a1358e6ac
Riya <riya.aneja07@gmail.com>
2025-04-10 00:56:19 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -1,37 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-void countFrequency(char *str, int *freq) {
-    while (*str) {
-        freq[(int)(*str)]++; 
-        str++;
-    }
-}
-bool areAnagrams(char *str1, char *str2) {
-    if (strlen(str1) != strlen(str2)) {
-        return false;
-    }
-    int freq1[256] = {0};
-    int freq2[256] = {0};
-    countFrequency(str1, freq1);
-    countFrequency(str2, freq2);
-    for (int i = 0; i < 256; i++) {
-        if (freq1[i] != freq2[i]) {
-            return false;
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str1[100],str2[100];
+    fgets(str1,sizeof(str1),stdin);
+    fgets(str2,sizeof(str2),stdin);
+    for(int i=0;i<strlen(str1);i++){
+        for(int j=i+1;j<strlen(str1)){
+            if(str1[i]>str1[j]){
+                char temp=str1[i];
+                str1[i]=str1[j];
+                str1[j]=temp;
+            }
         }
     }
-    return true;
-}
-int main() {
-    char str1[100], str2[100];
-    fgets(str1, sizeof(str1), stdin);
-    str1[strcspn(str1, "\n")] = 0;
-    fgets(str2, sizeof(str2), stdin);
-    str2[strcspn(str2, "\n")] = 0;
-    if (areAnagrams(str1, str2)) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
+    for(int i=0;i<strlen(str2);i++){
+        for(int j=i+1;j<strlen(str2)){
+                if(str2[i]>str2[j]){
+                    char temp=str2[i];
+                    str2[i]=str2[j];
+                    str2[j]=temp;
+                }
+            }
     }
+    if(strcmp(str1,str2)==0){
+        printf("Yes");
+    }
+    else printf("No");
     return 0;
 }
