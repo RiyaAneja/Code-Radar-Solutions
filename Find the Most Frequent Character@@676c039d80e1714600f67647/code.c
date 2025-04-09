98577e44772f235d5f343c1e0a6ee621eee3f65c
Riya <riya.aneja07@gmail.com>
2025-04-09 23:45:08 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,25 +1,49 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-int main() {
-    char str[1000];
-    int count[26] = {0}; 
-    int maxFreq = 0;
-    char result = '\0';
-    fgets(str, sizeof(str), stdin);
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (isalpha(str[i])) { 
-            char ch = tolower(str[i]); 
-            count[ch - 'a']++; 
-            if (count[ch - 'a'] > maxFreq || 
-                (count[ch - 'a'] == maxFreq && (result == '\0' || ch < result))) {
-                maxFreq = count[ch - 'a'];
-                result = ch;
+// #include <stdio.h>
+// #include <string.h>
+// #include <ctype.h>
+// int main() {
+//     char str[1000];
+//     int count[26] = {0}; 
+//     int maxFreq = 0;
+//     char result = '\0';
+//     fgets(str, sizeof(str), stdin);
+//     for (int i = 0; str[i] != '\0'; i++) {
+//         if (isalpha(str[i])) { 
+//             char ch = tolower(str[i]); 
+//             count[ch - 'a']++; 
+//             if (count[ch - 'a'] > maxFreq || 
+//                 (count[ch - 'a'] == maxFreq && (result == '\0' || ch < result))) {
+//                 maxFreq = count[ch - 'a'];
+//                 result = ch;
+//             }
+//         }
+//     }
+//     if (result != '\0') {
+//         printf("%c\n", result);
+//     } 
+//     return 0;
+// }
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int length=strlen(str);
+    char max;
+    int maxcount;
+    for(int i=0;i<length;i++){
+        int count=0;
+        for(int j=i+1;j<len;j++){
+            if(str[i]==str[j]){
+                count++;
             }
         }
+        if(maxcount<count){
+            maxcount=count;
+            max=str[i];
+            
+        }
+        printf("%c",max);
     }
-    if (result != '\0') {
-        printf("%c\n", result);
-    } 
-    return 0;
+    return 0;    
 }
