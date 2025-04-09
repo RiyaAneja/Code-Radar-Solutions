129f043cb590f76b8972cc5bc4636ea926b98d8c
Riya <riya.aneja07@gmail.com>
2025-04-10 00:41:22 +0530

    Solution to Replace a Character in a String

--- Replace a Character in a String@@676c039d80e1714600f67648/code.c
+++ Replace a Character in a String@@676c039d80e1714600f67648/code.c
@@ -1,30 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-void replaceCharacter(char *str, char toReplace, char replacement) {
-    while (*str) {
-        if (*str == toReplace) {
-            *str = replacement; 
+// #include <stdio.h>
+// #include <string.h>
+// void replaceCharacter(char *str, char toReplace, char replacement) {
+//     while (*str) {
+//         if (*str == toReplace) {
+//             *str = replacement; 
+//         }
+//         str++;
+//     }
+// }
+// int main() {
+//     char str[100]; 
+//     char toReplace, replacement;
+//     if (fgets(str, sizeof(str), stdin) == NULL) {
+//         fprintf(stderr, "Error reading input.\n");
+//         return 1;
+//     }
+//     str[strcspn(str, "\n")] = 0;
+//     if (scanf(" %c", &toReplace) != 1) {
+//         fprintf(stderr, "Error reading character to replace.\n");
+//         return 1; 
+//     }
+//     if (scanf(" %c", &replacement) != 1) {
+//         fprintf(stderr, "Error reading replacement character.\n");
+//         return 1; 
+//     }
+//     replaceCharacter(str, toReplace, replacement);
+//     printf("%s\n", str);
+//     return 0; 
+// }
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    char ch1,ch2;
+    scanf("%d",&ch1);
+    scanf("%d",&ch2);
+    for(int i=0;i<strlen(str);i++){
+        if(str[i]==ch1){
+            str[i]=ch2;
         }
-        str++;
     }
-}
-int main() {
-    char str[100]; 
-    char toReplace, replacement;
-    if (fgets(str, sizeof(str), stdin) == NULL) {
-        fprintf(stderr, "Error reading input.\n");
-        return 1;
-    }
-    str[strcspn(str, "\n")] = 0;
-    if (scanf(" %c", &toReplace) != 1) {
-        fprintf(stderr, "Error reading character to replace.\n");
-        return 1; 
-    }
-    if (scanf(" %c", &replacement) != 1) {
-        fprintf(stderr, "Error reading replacement character.\n");
-        return 1; 
-    }
-    replaceCharacter(str, toReplace, replacement);
-    printf("%s\n", str);
-    return 0; 
+    puts(str);
+    return 0;
 }
