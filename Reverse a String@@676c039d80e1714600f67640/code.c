7063a801eb16ca716b3c4942ebac4e9ece31c4fe
Riya <riya.aneja07@gmail.com>
2025-04-09 16:25:16 +0530

    Solution to Reverse a String

--- Reverse a String@@676c039d80e1714600f67640/code.c
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -1,26 +1,38 @@
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int main() {
-    char ch[50];
-    fgets(ch, sizeof(ch), stdin);
+// int main() {
+//     char ch[50];
+//     fgets(ch, sizeof(ch), stdin);
 
-    int size = 0;
-    int k = 0;
+//     int size = 0;
+//     int k = 0;
 
-    // Calculate the size of the string
-    while (ch[k] != '\0' && ch[k] != '\n') { // Check for both null terminator and newline
-        size++;
-        k++;
+//     // Calculate the size of the string
+//     while (ch[k] != '\0' && ch[k] != '\n') { // Check for both null terminator and newline
+//         size++;
+//         k++;
+//     }
+
+//     // Reverse the string
+//     for (int i = 0, j = size - 1; i < j; i++, j--) {
+//         char temp = ch[i];
+//         ch[i] = ch[j];
+//         ch[j] = temp;
+//     }
+
+//     puts(ch); // Print the reversed string
+//     return 0;
+// }
+#include<stdio.h>
+#include<String.h>
+int main(){
+    char str[50];
+    fgets(str,sizeof(str),stdin);
+    int len=strlen(str);
+    for(int i=0,j=len-1;i<j;i++,j--){
+        str[i]=str[j-i];
     }
-
-    // Reverse the string
-    for (int i = 0, j = size - 1; i < j; i++, j--) {
-        char temp = ch[i];
-        ch[i] = ch[j];
-        ch[j] = temp;
-    }
-
-    puts(ch); // Print the reversed string
+    puts(str);
     return 0;
 }
