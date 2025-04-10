032c1f97abb26a351c27867cf96ee09be7ece7bf
Riya <riya.aneja07@gmail.com>
2025-04-11 00:02:03 +0530

    Solution to Check if a String is Binary

--- Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
+++ Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
@@ -1,49 +1,19 @@
-// #include<stdio.h>
-// #include<string.h>
-// int main() {
-//     char str[100];
-//     int bin = 0;
-//     fgets(str, sizeof(str), stdin);
-//     for(int i = 0; i < strlen(str); i++) {
-//         if(str[i] == '0' || str[i] == '1') {
-//             bin=1;
-//             break; 
-//         }
-//     }
-//     if(bin == 1) 
-//         printf("Yes");
-//     else 
-//         printf("No");
-//     return 0;
-// }
-
-#include <stdio.h>
-#include <string.h>
-
+#include<stdio.h>
+#include<string.h>
 int main() {
     char str[100];
-    int isBinary = 0; // Flag to indicate if a binary digit is found
-
-    // Read a line of input
+    int bin = 0;
     fgets(str, sizeof(str), stdin);
-    
-    // Remove the newline character if present
-    str[strcspn(str, "\n")] = 0;
-
-    // Check for binary digits
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] == '0' || str[i] == '1') {
-            isBinary = 1; // Set flag if a binary digit is found
-            break; // Exit the loop early
+    for(int i = 0; i < strlen(str); i++) {
+        if(str[i] == '0' || str[i] == '1') {
+            bin++;
         }
+        else bin--;
     }
-
-    // Output result based on the flag
-    if (isBinary) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
-    }
-
+    if(bin >0) 
+        printf("Yes");
+    else 
+        printf("No");
     return 0;
 }
+
