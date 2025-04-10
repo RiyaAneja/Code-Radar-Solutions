bff156d863df0da7eb8285d51ed09e5eb88d4cd8
Riya <riya.aneja07@gmail.com>
2025-04-10 15:07:12 +0530

    Solution to Find the Longest Word in a String

--- Find the Longest Word in a String@@676c039d80e1714600f6764f/code.c
+++ Find the Longest Word in a String@@676c039d80e1714600f6764f/code.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX_LENGTH 1000
+
 int main() {
-    char str[MAX_LENGTH];
-    char longestWord[MAX_LENGTH] = "";
-    char currentWord[MAX_LENGTH] = "";
+    char str[1000]; // Directly using the size without #define
+    char longestWord[1000] = "";
+    char currentWord[1000];
+    int currentLength = 0;
+
+    // printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
+    
+    // Remove the newline character from the string if present
     str[strcspn(str, "\n")] = 0;
+
+    // Iterate through each character in the string
     for (int i = 0; ; i++) {
+        // If the character is not a space and not the end of the string
         if (str[i] != ' ' && str[i] != '\0') {
-            currentWord[strlen(currentWord)] = str[i];
-            currentWord[strlen(currentWord) + 1] = '\0'; 
+            currentWord[currentLength++] = str[i]; // Add character to current word
         } else {
-            if (strlen(currentWord) > strlen(longestWord)) {
-                strcpy(longestWord, currentWord);
+            // If we hit a space or end of string, check the current word
+            if (currentLength > strlen(longestWord)) {
+                currentWord[currentLength] = '\0'; // Null-terminate the current word
+                strcpy(longestWord, currentWord); // Update longest word
             }
-            currentWord[0] = '\0';
+            currentLength = 0; // Reset current length for the next word
         }
+        
+        // Break the loop if we reach the end of the string
         if (str[i] == '\0') {
             break;
         }
     }
-    if (strlen(currentWord) > strlen(longestWord)) {
-        strcpy(longestWord, currentWord);
+
+    // Final check in case the longest word is at the end of the string
+    if (currentLength > strlen(longestWord)) {
+        currentWord[currentLength] = '\0'; // Null-terminate the current word
+        strcpy(longestWord, currentWord); // Update longest word
     }
     printf("%s\n", longestWord);
     return 0;
