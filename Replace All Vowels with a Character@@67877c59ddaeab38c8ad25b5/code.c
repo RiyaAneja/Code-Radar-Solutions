40e4297fd6bc482d5a1abbd1e9d03cb8aeb2d937
Riya <riya.aneja07@gmail.com>
2025-04-11 00:12:44 +0530

    Solution to Replace All Vowels with a Character

--- Replace All Vowels with a Character@@67877c59ddaeab38c8ad25b5/code.c
+++ Replace All Vowels with a Character@@67877c59ddaeab38c8ad25b5/code.c
@@ -1,16 +1,45 @@
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char str[100];
+//     char replace;
+//     fgets(str,sizeof(str),stdin);
+//     scanf("%c",&replace);
+//     int length=strlen(str);
+//     for(int i=0;i<length;i++){
+//         if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+//             str[i]=replace;
+//         }
+//     }
+//     puts(str);
+//     return 0;
+// }
 #include<stdio.h>
 #include<string.h>
-int main(){
+
+int main() {
     char str[100];
     char replace;
-    fgets(str,sizeof(str),stdin);
-    scanf("%c",&replace);
-    int length=strlen(str);
-    for(int i=0;i<length;i++){
-        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
-            str[i]=replace;
+
+    // Reading the string input
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Removing the newline character
+
+    // Reading the replacement character
+    scanf("%c", &replace);
+
+    int length = strlen(str);
+
+    // Loop to replace vowels with the replacement character
+    for(int i = 0; i < length; i++) {
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || 
+           str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            str[i] = replace;
         }
     }
+
+    // Output the modified string
     puts(str);
+
     return 0;
 }
