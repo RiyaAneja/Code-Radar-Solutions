e8857536bdab71704fac4afebefd17fbc516eed8
Riya <riya.aneja07@gmail.com>
2025-04-11 01:20:29 +0530

    Solution to Remove All Consonants from a String

--- Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
+++ Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 #include<string.h>
-int main(){
+int main() {
     char str[100];
-    fgets(str,sizeof(str),stdin);
+    fgets(str, sizeof(str), stdin);
     str[strcspn(str, "\n")] = '\0';
-    int len= strlen(str);
-    for(int i=0;i<len;i++){
-        if(!(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')){
-            str[i]=' ';
+    int len = strlen(str);
+    int index = 0; 
+    for (int i = 0; i < len; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ||
+            !((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))) {
+            str[index++] = str[i];
         }
     }
+    str[index] = '\0';
     puts(str);
     return 0;
 }
