e77743907e1251fd1c27ac742a196d25e62cd5f8
Riya <riya.aneja07@gmail.com>
2025-04-11 16:30:50 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -1,44 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-
 int main() {
     char str[100];
-    printf("Enter a string: ");
     scanf("%s", str);
-    
     int len = strlen(str);
-    int start = 0; // Starting index of the longest palindrome
-    int maxLength = 1; // Length of the longest palindrome
-
-    // Check all substrings
+    int start = 0;
+    int maxLength = 1; 
     for (int i = 0; i < len; i++) {
         for (int j = i; j < len; j++) {
-            // Assume it's a palindrome
             int isPalindrome = 1;
-            // Check if the substring str[i..j] is a palindrome
             for (int left = i, right = j; left < right; left++, right--) {
                 if (str[left] != str[right]) {
-                    isPalindrome = 0; // Not a palindrome
-                    break; // Exit the loop early
+                    isPalindrome = 0;
+                    break; 
                 }
             }
-            // If it's a palindrome and longer than the previous longest
             if (isPalindrome) {
-                int currentLength = j - i + 1; // Length of the current palindrome
+                int currentLength = j - i + 1; 
                 if (currentLength > maxLength) {
-                    start = i; // Update starting index
-                    maxLength = currentLength; // Update max length
+                    start = i;
+                    maxLength = currentLength; 
                 }
             }
         }
     }
-
-    // Print the longest palindromic substring
-    printf("Longest palindromic substring: ");
     for (int i = start; i < start + maxLength; i++) {
-        putchar(str[i]); // Print each character
+        puts(str[i]); 
     }
-    putchar('\n'); // New line
-
     return 0;
 }
