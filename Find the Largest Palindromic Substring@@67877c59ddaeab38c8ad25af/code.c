ebf7d3ac48ba49001c2e5252fd22eef97421b718
Riya <riya.aneja07@gmail.com>
2025-04-11 16:27:26 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-    char str[100];    
+    char str[100];
+    printf("Enter a string: ");
     scanf("%s", str);
+    
     int len = strlen(str);
-    int start = 0, maxLength = 1;
+    int start = 0; // Starting index of the longest palindrome
+    int maxLength = 1; // Length of the longest palindrome
+
+    // Check all substrings
     for (int i = 0; i < len; i++) {
         for (int j = i; j < len; j++) {
+            // Assume it's a palindrome
             int isPalindrome = 1;
-            for (int k = 0; k <= (j - i) / 2; k++) {
-                if (str[i + k] != str[j - k]) {
-                    isPalindrome = 0;
-                    break;
+            // Check if the substring str[i..j] is a palindrome
+            for (int left = i, right = j; left < right; left++, right--) {
+                if (str[left] != str[right]) {
+                    isPalindrome = 0; // Not a palindrome
+                    break; // Exit the loop early
                 }
             }
-            if (isPalindrome && (j - i + 1) > maxLength) {
-                maxLength = j - i + 1;
-                start = i;
+            // If it's a palindrome and longer than the previous longest
+            if (isPalindrome) {
+                int currentLength = j - i + 1; // Length of the current palindrome
+                if (currentLength > maxLength) {
+                    start = i; // Update starting index
+                    maxLength = currentLength; // Update max length
+                }
             }
         }
     }
+
+    // Print the longest palindromic substring
+    printf("Longest palindromic substring: ");
     for (int i = start; i < start + maxLength; i++) {
-        printf("%c", str[i]);
+        putchar(str[i]); // Print each character
     }
-    printf("\n");
+    putchar('\n'); // New line
+
     return 0;
 }
