41577903d168bd6bab627a7939ae123cf36ec5b9
Riya <riya.aneja07@gmail.com>
2025-04-11 17:37:02 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,33 +1,54 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char str[100], sttr[100];
-    fgets(str, sizeof(str), stdin);
-    int length = strlen(str);
-    char max;
-    strcpy(sttr, str);
-    for(int i = 0; i < length; i++){
-        for(int j = i + 1; j < length; j++){
-            if(sttr[i] > sttr[j]){
-                char temp = sttr[i];
-                sttr[i] = sttr[j];
-                sttr[j] = temp;
-            }
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char str[100], sttr[100];
+//     fgets(str, sizeof(str), stdin);
+//     int length = strlen(str);
+//     char max;
+//     strcpy(sttr, str);
+//     for(int i = 0; i < length; i++){
+//         for(int j = i + 1; j < length; j++){
+//             if(sttr[i] > sttr[j]){
+//                 char temp = sttr[i];
+//                 sttr[i] = sttr[j];
+//                 sttr[j] = temp;
+//             }
+//         }
+//     }
+//     int maxcount = 0;
+//     for(int i = 0; i < length; i++){
+//         int count = 0;
+//         for(int j = 0; j < length; j++){
+//             if(str[i] == str[j]){
+//                 count++;
+//             }
+//         }
+//         if((count > maxcount) || (count == maxcount && str[i] < max)){
+//             maxcount = count;
+//             max = str[i];
+//         }
+//     }
+//     printf("%c", max);
+//     return 0;
+// }
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    char maxChar;
+    int maxCount = 0;
+    fgets(str, sizeof(str), stdin); 
+    str[strcspn(str, "\n")] = '\0'; 
+    for (int i = 0; str[i] != '\0'; i++) {
+        freq[(unsigned char)str[i]]++; 
+    }
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > maxCount) {
+            maxCount = freq[i];
+            maxChar = (char)i;
         }
     }
-    int maxcount = 0;
-    for(int i = 0; i < length; i++){
-        int count = 0;
-        for(int j = 0; j < length; j++){
-            if(str[i] == str[j]){
-                count++;
-            }
-        }
-        if((count > maxcount) || (count == maxcount && str[i] < max)){
-            maxcount = count;
-            max = str[i];
-        }
-    }
-    printf("%c", max);
+    printf("%c\n", maxChar);
     return 0;
 }
